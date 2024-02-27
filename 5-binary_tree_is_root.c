02b3faa0dd16fc72eba7a1557a027fe4d9a0c268
root <root@DESKTOP-CTTDNQL.localdomain>
2024-02-27 17:32:19 +0300

    fix

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,9 +1,15 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_is_root - Checks if a node is a root
+ *
+ * @node: Pointer to the node to check
+ *
+ * Return: 1 if @node is a root, 0 otherwise or if @node is NULL
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (!node || node->parent != NULL)
-    return (0);
+if (!node || node->parent != NULL)
+return (0);
 else
 return (1);
 }
