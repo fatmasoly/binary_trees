8af400bf46055017031243a3affb590455e56552
root <root@DESKTOP-CTTDNQL.localdomain>
2024-02-28 13:34:26 +0300

    eeee

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,7 +12,7 @@ if (tree == NULL)
 return (0);
 if (tree->left == NULL && tree->right == NULL)
 return (1);
-if(tree->left != NULL && tree->right != NULL)
+if (tree->left != NULL && tree->right != NULL)
 return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-return(0);
+return (0);
 }
